76a997b132a77a95086db282084e75e5da1d85bd
suleymanhummetli <suleymanhummetli8@gmail.com>
2026-04-04 22:39:44 +0400

    Add files via upload

---
+++ Script1.js
@@ -0,0 +1,8 @@
+
+const duyme = document.getElementById('duyme');
+const basliq = document.getElementById('basliq');
+
+duyme.addEventListener('click', function () {
+    basliq.style.color = 'red';
+    alert('Başlığın rəngi dəyişdirildi!');
+});
